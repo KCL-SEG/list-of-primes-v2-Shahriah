636530d4945e151c1a8108b2e2f8c13f0d5ed5e3
Shahriar Miah <shahriar.miah@kcl.ac.uk>
2023-10-01 14:58:50 +0100

    Complete Primes

--- primes.py
+++ primes.py
@@ -2,5 +2,22 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    list = [2]
+    counter = 3
+    checkPrime = True
+
+    while (len(list) < number_of_primes) and number_of_primes !=0:
+        for i in range(2,counter):
+            if (counter % i == 0) and (i != counter):
+                checkPrime = False
+        if checkPrime:
+            list.append(counter)
+            counter +=1
+        else:
+            checkPrime = True
+            counter+=1
+    if number_of_primes <=0:
+        raise ValueError
+    print(list)
     return list
+
